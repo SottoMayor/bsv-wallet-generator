655956cab3c041b1c42f2858403be44aa4064e22
David Sotto Mayor <ddsmmf@gmail.com>
2024-09-09 13:27:12 -0400

    Method to write the wallet in the pos. file

--- src/bsv/wallet-writer.ts
+++ src/bsv/wallet-writer.ts
@@ -1,4 +1,5 @@
 import { WalletString } from "../shared/types";
+import * as fs from 'fs';
 
 export default class WalletFileWriter {  
     constructor(private filePath: string) {}
@@ -15,5 +16,17 @@ export default class WalletFileWriter {
       const pk = record.pk.padEnd(66, ' '); // pk -> 66 characters
       const address = record.address.padEnd(35, ' '); // address -> 35 characters
       return `${sk} ${pk} ${address}`;
+    }
+  
+    public writeWallet(records: WalletString[]): void {
+      const stream = fs.createWriteStream(this.filePath);
+  
+      stream.write(this.createHeader() + '\n');
+
+      records.forEach(singleWallet => {
+        stream.write(this.createRecord(singleWallet) + '\n');
+      });
+  
+      stream.end();
     }
 }
