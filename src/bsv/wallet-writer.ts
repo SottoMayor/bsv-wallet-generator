41c6545652028a57df23c72e6eafe32908d72d3d
David Sotto Mayor <ddsmmf@gmail.com>
2024-09-09 13:24:41 -0400

    CreateHeader method

--- src/bsv/wallet-writer.ts
+++ src/bsv/wallet-writer.ts
@@ -1,3 +1,10 @@
 export default class WalletFileWriter {  
     constructor(private filePath: string) {}
+
+     private createHeader(): string {
+      const sk = 'secret key'.padEnd(80, ' ');
+      const pk = 'public key'.padEnd(66, ' ');
+      const address = 'address'.padEnd(35, ' ');
+      return `${sk} ${pk} ${address}`;
+    }  
 }
