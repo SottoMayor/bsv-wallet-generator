17b32fca0fbc4b52575a74f362aed897d6135287
David Sotto Mayor <ddsmmf@gmail.com>
2024-09-09 13:28:36 -0400

    Generating a wallet as a positional file

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,20 @@
 import dotenv from 'dotenv';
 import BsvApi from './bsv/bsv-api';
 import { EnvType, WalletString } from './shared/types';
+import WalletFileWriter from './bsv/wallet-writer';
 dotenv.config();
 const NETWORK = process.env.NETWORK! as EnvType;
 const bsvApi = new BsvApi(NETWORK);
 
+
+// Generating the random wallet
 const randomWallet: Array<WalletString> = [];
 for(let counter = 0; counter < 4; counter++) {
     const newWallet = bsvApi.randomWallet().serialize('string');
     randomWallet.push(newWallet);
 }
-
 console.log(JSON.stringify(randomWallet));
+
+// Record in Positional File
+const writer = new WalletFileWriter('wallet.txt');
+writer.writeWallet(randomWallet);
