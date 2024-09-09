2b449d3c8a340952d75102d0b01593244e7cefc1
David Sotto Mayor <ddsmmf@gmail.com>
2024-09-08 22:08:24 -0400

    Refactoring the BsvApi class

--- src/bsv-api.ts
+++ src/bsv-api.ts
@@ -1,10 +1,15 @@
-import { bsv } from 'scrypt-ts';
+import { bsv, Network } from 'scrypt-ts';
+import { EnvType } from './shared/types';
 
 export default class BsvApi {
-    constructor(readonly env: string){}
+    constructor(readonly env: EnvType = 'testnet') {}
+
+    private network(): Network {
+        return bsv.Networks.get(this.env);
+    }
 
     public randomSecretKey(): string {
-        const privKey = bsv.PrivateKey.fromRandom().toString()
+        const privKey = bsv.PrivateKey.fromRandom(this.network()).toString();
         return privKey;
     }
 }
