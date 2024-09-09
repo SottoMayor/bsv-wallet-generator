d1e70c6ef5db93478f16229346a2ca534abb84bd
David Sotto Mayor <ddsmmf@gmail.com>
2024-09-09 12:57:33 -0400

    Creating wallet types

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -2,4 +2,6 @@ import { bsv } from 'scrypt-ts';
 
 export type EnvType = 'mainnet' | 'testnet';
 
-export type RandomWallet = { sk: bsv.PrivateKey, pk: bsv.PublicKey, address: bsv.Address }
+export type Wallet = { sk: bsv.PrivateKey, pk: bsv.PublicKey, address: bsv.Address }
+
+export type WalletString = { sk: string, pk: string, address: string }
