28ccf0c33a7a72f8723fc5840060358658858b4b
David Sotto Mayor <ddsmmf@gmail.com>
2024-09-08 21:44:31 -0400

    Installing and config the dotenv dependency

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
-import { bsv } from 'scrypt-ts'
+import { bsv } from 'scrypt-ts';
+import dotenv from 'dotenv';
+dotenv.config();
+const NETWORK = process.env.NETWORK;
 
 const privKey = bsv.PrivateKey.fromRandom()
 
-console.log(privKey);
+console.log(privKey, NETWORK);
