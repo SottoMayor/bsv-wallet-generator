15f579fc4cf7deff9f17eceb63014b95ebd05866
David Sotto Mayor <ddsmmf@gmail.com>
2024-09-08 22:08:46 -0400

    Applying the env type

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import dotenv from 'dotenv';
 import BsvApi from './bsv-api';
+import { EnvType } from './shared/types';
 dotenv.config();
-const NETWORK = process.env.NETWORK!;
+const NETWORK = process.env.NETWORK! as EnvType;
 const bsvApi = new BsvApi(NETWORK);
 
 console.log(bsvApi.randomSecretKey());
